576442b65345f6b754e5273684c97c25f4826764
sulaiman96 <sulaimanmaarij@gmail.com>
2019-06-20 16:12:02 +0100

    working navigation

--- app/src/main/java/project/swoosh/MainActivity.kt
+++ app/src/main/java/project/swoosh/MainActivity.kt
@@ -1,12 +1,20 @@
 package project.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findButton.setOnClickListener(){
+            val scrimmageIntent = Intent(this, ScrimmageDetails::class.java)
+            startActivity(scrimmageIntent)
+        }
     }
+
 }
