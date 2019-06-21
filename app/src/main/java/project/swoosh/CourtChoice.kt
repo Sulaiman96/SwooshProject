05fc6f847baf062e9e052865f48951d1c884e20f
sulaiman96 <sulaimanmaarij@gmail.com>
2019-06-21 11:55:57 +0100

    3 working screens

---
+++ app/src/main/java/project/swoosh/CourtChoice.kt
@@ -0,0 +1,12 @@
+package project.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourtChoice : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_court_choice)
+    }
+}
--- app/src/main/java/project/swoosh/ScrimmageDetails.kt
+++ app/src/main/java/project/swoosh/ScrimmageDetails.kt
@@ -1,12 +1,18 @@
 package project.swoosh
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_scrimmage_details.*
 
-class ScrimmageDetails : AppCompatActivity() {
+class ScrimmageDetails : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scrimmage_details)
+
+        nextButton.setOnClickListener(){
+            val scrimmageIntent = Intent(this, CourtChoice::class.java)
+            startActivity(scrimmageIntent)
+        }
     }
 }
